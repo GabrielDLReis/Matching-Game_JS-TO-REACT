b82fcceb6ff2453b7463f6f0b5876443f5eea394
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-05-10 12:07:26 -0300

    First commit with all datas

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const CardGame = () => {
+  return `
+  <article class="card-game">
+    <img src="images/alura-pixel.png" alt="logo da alura">
+  </article>
+  `
+}
+
+export default CardGame
