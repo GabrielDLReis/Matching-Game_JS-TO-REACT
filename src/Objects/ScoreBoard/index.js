10783aa4c2019499cc551d7dd19a89d11d68e18f
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-05-13 11:58:57 -0300

    Add ScoreBoard CSS

--- src/Objects/ScoreBoard/index.js
+++ src/Objects/ScoreBoard/index.js
@@ -1,11 +1,11 @@
 import PlayerName from '../../components/PlayerName'
+import './styles.css'
 
 function ScoreBoard() {
   return /*html*/ `
   <header class='score-board'>
     ${PlayerName('Player1')}
-    ${PlayerName('Player2')}
-  
+    ${PlayerName('Player2')}  
   </header>
   `
 }
