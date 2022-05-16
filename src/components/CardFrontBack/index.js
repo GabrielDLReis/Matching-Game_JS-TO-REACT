097b381fab7e0b8bd9997d1fbadf800121ed7ce2
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-05-16 13:51:48 -0300

    Added a new div to can do the back and front flip animation

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from '../CardGame'
+import './styles.css'
 
 function CardFrontBack() {
   return /*html*/ `
     <article class='card-front-back'>
-      ${CardGame()}
-      ${CardGame('javascript', 'logo do Javascript')}
+      <div class="card -front">
+        ${CardGame()}
+      </div>
+      <div class="card -back">
+        ${CardGame('javascript', 'logo do Javascript')}
+      </div>
     </article>
   `
 }
