805cb63e5068c3d65a9d27662681345ddf71da9d
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-05-20 12:39:05 -0300

    Added arrow image component in ScoreBoard

--- src/Objects/ScoreBoard/index.js
+++ src/Objects/ScoreBoard/index.js
@@ -3,10 +3,12 @@ import './styles.css'
 
 import VsPlayer from '../../components/VsPlayer'
 import PlayerScore from '../../components/PlayerScore'
+import ArrowDown from '../../components/ArrowDown'
 
 function ScoreBoard() {
   return /*html*/ `
   <header class='score-board'>
+    ${ArrowDown()}  
     ${PlayerName('Player1')}
     ${PlayerScore(3)}
     ${VsPlayer()}
---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,7 @@
+function ArrowDown() {
+  return /*html */ `
+    <img src="images/arrow.png" alt="Ícone de uma seta para baixo" />
+  `
+}
+
+export default ArrowDown
