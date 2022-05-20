c9025bc709187b89550ba9aad7cb78d2bfc12f58
GabrielDLR <gabrieldilucciareis@hotmail.com>
2022-05-20 14:35:21 -0300

    Data with informations of cards added in BoardGame

---
+++ src/Objects/BoardGame/data.js
@@ -0,0 +1,28 @@
+const cards = [
+  {
+    icon: 'html',
+    altIcon: 'logo do HTML'
+  },
+  {
+    icon: 'html',
+    altIcon: 'logo do html'
+  },
+  {
+    icon: 'javascript',
+    altIcon: 'logo do javascript'
+  },
+  {
+    icon: 'javascript',
+    altIcon: 'logo do javascript'
+  },
+  {
+    icon: 'css',
+    altIcon: 'logo do css'
+  },
+  {
+    icon: 'css',
+    altIcon: 'logo do css'
+  }
+]
+
+export default cards
